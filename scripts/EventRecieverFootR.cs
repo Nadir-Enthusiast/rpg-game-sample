310da786f228510a7d7538e705f21c7fc5eb26a0
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2023-05-30 05:20:17 -0400

    Create EventRecieverFootR.cs

---
+++ scripts/EventRecieverFootR.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventRecieverFootR : MonoBehaviour
+{
+    public void NewEvent()
+    {
+
+    }
+
+    public void FootR()
+    {
+
+    }
+
+    public void StopRolling()
+    {
+
+    }
+
+    public void JumpEvent()
+    {
+
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public void Land()
+    {
+
+    }
+
+    public void Punch()
+    {
+
+    }
+}
